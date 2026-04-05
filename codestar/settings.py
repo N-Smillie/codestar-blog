cca0518e32636c1cd0900ed2e93dcd1252bcfcbc
N-Smillie <dominategamehs@gmail.com>
2026-04-05 11:13:31 +0100

    added js directory

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = 'django-insecure-nrm(q6!v(b9)co#w)@#x-4q_(*sggxic(9_^78_e=qn7^dv=jq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com',
                  '127.0.0.1',]
