ede3b924060820c4e86cf9ea63a88abc6f56008a
N-Smillie <dominategamehs@gmail.com>
2026-04-09 09:11:20 +0100

    fixed api key

--- env.py
+++ env.py
@@ -4,4 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_Icqt8HLDip9v@ep-cool-union-agf0q2ks.c-2.eu-central-1.aws.neon.tech/aroma_lasso_islam_772380")
 
 os.environ.setdefault(
-    "CLOUDINARY_URL", "cloudinary://<your_api_key>:<your_api_secret>@dlrkvmgyh")
+    "CLOUDINARY_URL", "cloudinary://154463822131721:Ffeq-DnFophCH9Als67oQewYTcg@dlrkvmgyh")
